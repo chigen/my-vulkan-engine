5a4f3f10d18a15030479d6f44eebf1a6a2ba067d
chigen <1173279688@qq.com>
2024-02-27 14:42:47 +0900

    fix camera's setViewYXZ function

--- engine/camera.cpp
+++ engine/camera.cpp
@@ -67,16 +67,19 @@ namespace engine{
         const glm::vec3 u{(c1 * c3 + s1 * s2 * s3), (c2 * s3), (c1 * s2 * s3 - c3 * s1)};
         const glm::vec3 v{(c3 * s1 * s2 - c1 * s3), (c2 * c3), (c1 * c3 * s2 + s1 * s3)};
         const glm::vec3 w{(c2 * s1), (-s2), (c1 * c2)};
-
+        
         view = glm::mat4{1.f};
-        view = {
-            u.x, u.y, u.z, 0,
-            v.x, v.y, v.z, 0,
-            w.x, w.y, w.z, 0,
-            -glm::dot(u, position),
-            -glm::dot(v, position),
-            -glm::dot(w, position),
-            1
-        };
+        view[0][0] = u.x;
+        view[1][0] = u.y;
+        view[2][0] = u.z;
+        view[0][1] = v.x;
+        view[1][1] = v.y;
+        view[2][1] = v.z;
+        view[0][2] = w.x;
+        view[1][2] = w.y;
+        view[2][2] = w.z;
+        view[3][0] = -glm::dot(u, position);
+        view[3][1] = -glm::dot(v, position);
+        view[3][2] = -glm::dot(w, position);
     }
 }
